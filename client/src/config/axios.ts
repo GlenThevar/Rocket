fd9de124a11f33644731d711aa1acaeb3c352a75
Glenthevar <glenthevar1@gmail.com>
2026-04-19 16:08:40 +0530

    changed the axios file to fix render issues

--- client/src/config/axios.ts
+++ client/src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/" : "",
+    baseURL: import.meta.env.VITE_BASE_URL || "/api",
     withCredentials: true,
 });
 
